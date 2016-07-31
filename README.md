fe47e5aa376a6d36799ed5b1744ff684e683870d
Ivo Janssen <foxxyz@gmail.com>
2016-07-31 13:14:43 -0700

    Added LICENSE, better README, output basic sensor readings

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
-Sensor Watch Web Monitor
-========================
+SensorWatch Server & Web Monitor
+================================
 
-Web Monitor for showing sensorwatch data.
+SensorWatch server for data collection & web monitor for data visualization.
 
 Requirements
 ------------
 
- * Node.js (install package `nodejs` with your favorite package manager)
+ * MongoDB (package `mongodb`)
+ * Node.js (package `nodejs`)
 
 Installation
 ------------
 
 1. Clone project: `git clone <url>`
 2. Install dependencies: `npm install`
+
+Usage
+-----
+
+Be sure to specify a custom secret token for security reasons. This should then be used by every client to post updates.
+
+For deployment, specify this in your environment variables. For development, you can do:
+
+`SENSORWATCH_SECRET=123456 node index.js`
--- index.js
+++ index.js
@@ -1,28 +1,104 @@
 var WebSocketServer = require("ws").Server
+var MongoClient = require('mongodb').MongoClient
 var http = require("http")
 var express = require("express")
+var bodyParser = require("body-parser")
 var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + "/"))
+// Set up DB
+var url = 'mongodb://localhost:27017/test';
+var DB = null
+MongoClient.connect(url, function(err, db) {
+    if (err) {
+        console.error("Error connecting to DB: " + err)
+        return
+    }
+    console.info("Successfully connected to DB")
+    DB = db
+})
 
+// Set up express server
 var server = http.createServer(app)
-server.listen(port)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-console.log("http server listening on %d", port)
+// Accept GET requests on /
+app.get('/', (req, res) => {
+    // One hour ago
+    var since = new Date(new Date() - 7200 * 1000)
+    var cursor = DB.collection('sensors').find({ timestamp: { $gt: since}})
+    var results = []
+    cursor.each((err, doc) => {
+        if (!doc) {
+            res.render('index', {results: results})
+            return
+        }
+        var docDate = new Date(doc.timestamp)
+        Object.keys(doc.values).forEach((minute) => {
+            Object.keys(doc.values[minute]).forEach((second) => {
+                var resultDate = new Date(docDate.getTime() + parseInt(minute) * 60 * 1000 + parseInt(second) * 1000)
+                results.push([resultDate.getTime(), doc.values[minute][second]])
+            })
+        })
+    })
+})
 
+// Accept incoming POST requests on /update
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/update/:secret/', (req, res) => {
+    // Make sure secret token matches, otherwise ignore
+    if (req.params.secret !== process.env['SENSORWATCH_SECRET']) {
+        error404(req, res)
+    }
+
+    // Get current timestamp
+    var now = new Date()
+    var minute = now.getMinutes()
+    now.setMinutes(0)
+    var second = now.getSeconds()
+    now.setSeconds(0)
+    now.setMilliseconds(0)
+
+    // Document time values to update
+    var docID = req.body.id + ':' + now.valueOf()
+    var doc = DB.collection('sensors').find({_id: docID}).limit(1).next()
+        .then((doc) => {
+            if (doc) DB.collection('sensors').update({_id: docID}, {$set: {["values." + minute + "." + second]: parseFloat(req.body.value)}})
+            else DB.collection('sensors').insert({
+                _id: docID,
+                timestamp: now,
+                id: req.body.id,
+                unit: req.body.unit,
+                values: {[minute]: {[second]: parseFloat(req.body.value)}}
+            })
+        })
+    res.status(201).send('success')
+})
+
+// Everything else 404's
+app.use(error404)
+function error404(req, res, next) {
+    res.status(404).send('Not found.')
+}
+
+// Set up websocket server
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  })
+    }, 1000)
 
-  console.log("websocket connection open")
+    console.log("Websocket client connected")
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+    ws.on("close", function() {
+        console.log("Connection closed")
+        clearInterval(id)
+    })
+})
+
+// Start listening
+server.listen(port, () => {
+    console.log('SensorWatch Monitor started on ' + server.address().port)
 })
